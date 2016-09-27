699b7c7308c67bf45956e6b423c2eb6f0a92a0d2
Jesse Shawl <jesse@jshawl.com>
2016-09-27 04:51:22 -0400

    binge code

--- diff.js
+++ diff.js
@@ -1,3 +1,60 @@
 var diff = (function(){
-  return function(){}
-})
+  return function(content){
+    return {
+      hunks: hunks(content),
+      toHTML: function(){
+        return this.hunks.map(hunk => {
+          return "<h2>"+hunk.filename+":"+hunk.lineRange.from[0]+"</h2>" + hunk.content.split("\n").map(line=>{
+            line = line.replace('<','&lt;')
+            if(line.match(/^-/))
+              return "<div class='minus'>" + line + "</div>"
+            if(line.match(/^\+/))
+              return "<div class='plus'>" + line + "</div>"
+            return "<div>" + line + "</div>"
+          }).join("")
+        }).join("<hr>")
+      }
+    }
+  }
+
+  function filename(str){
+    var names = str.split("\n").filter((line) => {
+      return line.match(/^[-+]{3}/)
+    }).map(function(name){
+      return name.substr(6)
+    })
+    return names[0]
+  }
+  function lineRange(str){
+    var matches = str.match(/@@ (.*) @@\n/)[1].split(" ")
+    var from = matches[0].substr(1).split(",").map(Number)
+    var to = matches[1].substr(1).split(",").map(Number)
+    return {
+      from: from,
+      to: to
+    }
+  }
+  function hunks(str){
+    var lines = str.split("\n")
+    var hs = []
+    var hunk = []
+    for(var i = 0; i < lines.length; i++){
+      if(lines[i].match(/^diff --git/) && i != 0){
+        hs.push(hunk.join("\n"))
+        hunk = []
+      }
+      hunk.push(lines[i])
+    }
+    hs.push(hunk.join("\n"))
+    return hs.map( h => {
+      return {
+        content: h.split("\n").filter(line=>{
+          return !line.match(/^(diff --git|index|---|\+\+\+|@@)/)
+        }).join("\n"),
+        filename: filename(h),
+        lineRange: lineRange(h)
+      }
+    })
+    return hs
+  }
+})()
--- test/spec/DiffSpec.js
+++ test/spec/DiffSpec.js
@@ -1,5 +1,47 @@
+const content = `diff --git first
+index bcfe2e7..5468958 100644
+--- a/diff.js
++++ b/diff.js
+@@ -1,3 +1,25 @@
+ var diff = (function(){
+-  return function(){}
+-})
+diff --git second
+index 926aa1d..91c015a 100644
+--- a/test/index.html
++++ b/test/index.html
+@@ -11,15 +11,12 @@
+   <script src="lib/jasmine-2.5.2/jasmine-html.js"></script>
+   <script src="lib/jasmine-2.5.2/boot.js"></script>
+
+-  <!-- include source files here... -->
+diff --git third
+index 81bb922..fa6b03c 100644
+--- a/test/spec/DiffSpec.js
++++ b/test/spec/DiffSpec.js
+@@ -1,5 +1,37 @@
+ describe("Diff", function(){
++  var d = diff(content)`
+
 describe("Diff", function(){
+  var d = diff(content)
   it("is a function", function(){
     expect(typeof diff).toBe("function")
+  })
+  it("has hunks", function(){
+    expect(d.hunks.length).toBe(3)
+  })
+  it("has a filename", function(){
+    expect(d.hunks[0].filename).toBe("diff.js")
+  })
+  it("has a line range", function(){
+    expect(d.hunks[0].lineRange.from[0]).toBe(1)
+    expect(d.hunks[0].lineRange.from[1]).toBe(3)
+    expect(d.hunks[0].lineRange.to[0]).toBe(1)
+    expect(d.hunks[0].lineRange.to[1]).toBe(25)
+  })
+  it("has a toHTML() method", function(){
+    var html = d.toHTML()
+    console.log(html)
   })
 })
